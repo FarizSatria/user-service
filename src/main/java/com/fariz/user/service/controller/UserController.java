35606a96f0145f3f1a323f2e6771d7ea73360674
FarizSatria <farizsatria18@gmail.com>
2022-10-06 11:16:22 +0700

    user-service

--- src/main/java/com/fariz/user/service/controller/UserController.java
+++ src/main/java/com/fariz/user/service/controller/UserController.java
@@ -8,6 +8,7 @@ import com.fariz.user.service.dto.User;
 import com.fariz.user.service.service.UserService;
 import java.net.URI;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,13 +16,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 /**
  *
  * @author Acer
  */
+@RestController
 public class UserController {
+    @Autowired
     private UserService userService;
     
     @GetMapping("/user")
@@ -38,7 +42,7 @@ public class UserController {
     public ResponseEntity<Object> addUser(@RequestBody User user){
         userService.addUser(user);
         URI path = ServletUriComponentsBuilder.fromCurrentRequest()
-                .path("/id")
+                .path("/{id}")
                 .buildAndExpand(user.getId())
                 .toUri();
         return ResponseEntity.created(path).build();
